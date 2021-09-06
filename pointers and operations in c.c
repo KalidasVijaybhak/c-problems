0b377741052a868558fc9ceaec29cd276c81791c
Kalidas <kalidasvijayabhak@gmail.com>
2021-09-06 10:21:19 +0530

    success

--- pointers and operations in c.c
+++ pointers and operations in c.c
@@ -2,10 +2,23 @@
 
 int main(){
 
-int x;
-int *y = &x;
+int x = 2;
+int *y;
+y = &x;
+int z = 2;
+int *k ;
+k = &z;
 
-printf()
+printf("the value of memory address of x =5 is %p",(void *)y);
+
+printf("\nthe value of memory address of x =5 is %d",*y);
 
 
+
+printf("\nthe value of memory address of z =5 is %p",(void *)k);
+
+printf("\nthe value of memory address of z =5 is %d",*k);
+
+
+printf("the sum of *z + *x = %d",*k + *y);
 }
