ffe6f3928c7ccb653a5be7939e14154353396c15
Kalidas <kalidasvijayabhak@gmail.com>
2021-09-19 23:21:46 +0530

    Solved #4

--- struct arrays and reading string.c
+++ struct arrays and reading string.c
@@ -1,6 +1,7 @@
-
+//for doubts use this youtube.com/watch?v=lXYSW4_ojEk
 #include <stdio.h>
-#include <math.h>    
+#include <math.h> 
+#include<string.h>
 struct EmployeeData
 {
   char Name[100];
@@ -18,14 +19,30 @@ int main()
    int i = 0;
   for(int i = 1;i <= x;i++){
        printf("Enter data of Employee %d \n",i);
-       printf("Name: ");
-     scanf("%s",e1[i].Name);
+       
        printf("ID: ");
      scanf("%d",&e1[i].id);
+     fgetc(stdin);
+     printf("Name: ");
+     fgets(e1[i].Name,100,stdin);
+     e1[i].Name[strlen(e1[i].Name)-1]=0;
+       
        printf("Salary: ");
      scanf("%f",&e1[i].salary);
-  }
-    printf("-----Registered Employees-----\n\n");
+     fgetc(stdin);
+   }
+
+ // for(int i = 1;i <= x;i++){
+ //       printf("Enter data of Employee %d \n",i);
+ //       printf("Name: ");
+ //     scanf("%s",e1[i].Name);
+ //       printf("ID: ");
+ //     scanf("%d",&e1[i].id);
+ //       printf("Salary: ");
+ //     scanf("%f",&e1[i].salary);
+ //  }
+
+       printf("-----Registered Employees-----\n\n");
     for(int i = 1;i <= x;i++){
        printf("Data of Employee %d \n",i);
        printf("Name: %s \n",e1[i].Name);
