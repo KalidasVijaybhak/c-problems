d5af1732dca055d8997f82d99af42361ad07d8d6
Kalidas <kalidasvijayabhak@gmail.com>
2021-09-26 21:52:28 +0530

    updated

--- infix and postfix.c
+++ infix and postfix.c
@@ -5,7 +5,7 @@ int main() {
    // 5 is displayed
    // Then, var1 is increased to 6.
    printf("%d\n", var1++);
-
+   
    // var2 is increased to 6 
    // Then, it is displayed.
    printf("%d\n", ++var2);
